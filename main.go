5ed548720d948208ee4e3874c2bb8ad7a3facb53
Jay At Work <42274740+jayatwork@users.noreply.github.com>
2019-01-12 22:05:59 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-placeholder
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	log.Fatal(http.ListenAndServe(":8050", nil))
+}
